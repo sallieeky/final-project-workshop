9e51884912b32ecbd487baf4366d4e334f2e4891
sallieeky <sallieeky@gmail.com>
2024-12-19 21:42:19 +0800

    🔥 test(InventoryServicesTest.php): remove unnecessary code and comment in the 'it can print' test

--- tests/Feature/InventoryServicesTest.php
+++ tests/Feature/InventoryServicesTest.php
@@ -5,8 +5,5 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 uses(RefreshDatabase::class);
 
 test('it can print', function () {
-    $a = app(\App\Domains\Inventories\Services\InventoryService::class)->updatingStock(1, 1);
-
-    // expect error UnexpectedValueException
-    expect($a)->toThrow(\UnexpectedValueException::class);
+    // ...
 });
