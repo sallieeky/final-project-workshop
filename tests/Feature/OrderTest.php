64ba1698733b41799662391056e90364164db17f
sallieeky <sallieeky@gmail.com>
2024-12-20 08:47:06 +0800

    🔧 fix(OrderTest.php): fix incorrect assertion message in test case ✨ feat(OrderTest.php): add test case to check if order creation fails with insufficient stock

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -30,12 +30,34 @@ test('it can create order', function () {
     try {
         $result = app(\App\Domains\Orders\Services\OrderService::class)->store($data);
         expect($result->product_id)->toBe($product->id)
-            ->and($result->quantity)->toBe(1)
+            ->and($result->quantity)->toBe(15)
             ->and($inventory1->refresh()->stock)->toBe(0)
             ->and($inventory2->refresh()->stock)->toBe(5);
+    } catch (\Exception $e) {
+        expect($e->getMessage())->toBeNull();
+    }
+});
 
+test('it cannot create order with insufficient stock', function () {
+    $product = \App\Models\Product::factory()->create();
+    $inventory1 = \App\Models\Inventory::factory()->create([
+        'product_id' => $product->id,
+        'warehouse_id' => \App\Models\Warehouse::factory()->create()->id,
+        'stock' => 10,
+    ]);
+    $inventory2 = \App\Models\Inventory::factory()->create([
+        'product_id' => $product->id,
+        'warehouse_id' => \App\Models\Warehouse::factory()->create()->id,
+        'stock' => 10,
+    ]);
+    $data = new \App\Domains\Orders\Data\OrderData(
+        product_id: $product->id,
+        quantity: 25,
+    );
+
+    try {
         app(\App\Domains\Orders\Services\OrderService::class)->store($data);
     } catch (\Exception $e) {
-        expect($e->getMessage())->toBe('Failed asserting that 15 is identical to 1.');
+        expect($e->getMessage())->toBe('Insufficient stock! Exist only:20 but required:25');
     }
 });
